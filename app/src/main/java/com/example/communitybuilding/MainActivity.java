e3459acb65dceb8e50857f3fa9e47e7f9479a949
nanjibac <77299533+nanjibac@users.noreply.github.com>
2022-02-13 00:30:20 -0500

    the notification bar

--- app/src/main/java/com/example/communitybuilding/MainActivity.java
+++ app/src/main/java/com/example/communitybuilding/MainActivity.java
@@ -13,33 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button teen_btn = (Button) findViewById(R.id.TeenButton);
-        teen_btn.setOnClickListener(new View.OnClickListener()
-        {
-            public void onClick(View v)
-            {
-                startActivity(new Intent(MainActivity.this, Teen_page.class));
-            }
-        });
-
-        Button adult_btn = (Button) findViewById(R.id.AdultButton);
-        adult_btn.setOnClickListener(new View.OnClickListener()
-        {
-            public void onClick(View v)
-            {
-                startActivity(new Intent(MainActivity.this, Teen_page.class));
-            }
-        });
-
-        Button senior_btn = (Button) findViewById(R.id.SeniorButton);
-        senior_btn.setOnClickListener(new View.OnClickListener()
-        {
-            public void onClick(View v)
-            {
-                startActivity(new Intent(MainActivity.this, Teen_page.class));
-            }
-        });
     }
+
 }
 
