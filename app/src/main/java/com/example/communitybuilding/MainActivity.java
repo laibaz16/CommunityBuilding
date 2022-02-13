3a22d2880ed399bbd868777ab8bddd1a0b33f30b
laibaz16 <laibazahid16@gmail.com>
2022-02-12 19:42:56 -0500

    Front end of the Teen activities

--- app/src/main/java/com/example/communitybuilding/MainActivity.java
+++ app/src/main/java/com/example/communitybuilding/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.communitybuilding;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button teen_btn = (Button) findViewById(R.id.TeenButton);
+        teen_btn.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                startActivity(new Intent(MainActivity.this, Teen_page.class));
+            }
+        });
+
+        Button adult_btn = (Button) findViewById(R.id.AdultButton);
+        adult_btn.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                startActivity(new Intent(MainActivity.this, Teen_page.class));
+            }
+        });
+
+        Button senior_btn = (Button) findViewById(R.id.SeniorButton);
+        senior_btn.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                startActivity(new Intent(MainActivity.this, Teen_page.class));
+            }
+        });
     }
+}
+
 }
