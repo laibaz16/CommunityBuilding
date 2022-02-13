0cf56832177feea9305780e4cf69c108251b45d2
laibaz16 <laibazahid16@gmail.com>
2022-02-13 01:47:58 -0500

    Final Front end of the Adult page

--- app/src/main/java/com/example/communitybuilding/Adult_page.java
+++ app/src/main/java/com/example/communitybuilding/Adult_page.java
@@ -2,7 +2,11 @@ package com.example.communitybuilding;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class Adult_page extends AppCompatActivity {
 
@@ -11,4 +15,38 @@ public class Adult_page extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adult_page);
     }
+
+    public void fitnessclick(View v) {
+
+        TextView fitness_text = (TextView) findViewById(R.id.fitness);
+        fitness_text.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(Adult_page.this, test.class));
+            }
+        });
+        ImageButton fitnessimage = (ImageButton) findViewById(R.id.fitnessimage);
+        fitnessimage.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(Adult_page.this, test.class));
+            }
+        });
+    }
+
+    public void mentalhealthclick(View v) {
+
+        TextView healthtext = (TextView) findViewById(R.id.mentalhealth);
+        healthtext.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(Adult_page.this, test.class));
+            }
+        });
+        ImageButton healthimage = (ImageButton) findViewById(R.id.mentalhealthimage);
+        healthimage.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(Adult_page.this, test.class));
+            }
+        });
+    }
+
+
 }
