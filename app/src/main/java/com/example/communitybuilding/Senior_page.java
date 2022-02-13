2c1bda9535193eafdbbc93aaf8034d252ab4380a
laibaz16 <laibazahid16@gmail.com>
2022-02-13 03:58:49 -0500

    Final Front end of the Adult page. Fixed errors for gradle

--- app/src/main/java/com/example/communitybuilding/Senior_page.java
+++ app/src/main/java/com/example/communitybuilding/Senior_page.java
@@ -2,7 +2,11 @@ package com.example.communitybuilding;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class Senior_page extends AppCompatActivity {
 
@@ -11,4 +15,52 @@ public class Senior_page extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_senior_page);
     }
+    public void exclick(View v) {
+
+        TextView extext = (TextView) findViewById(R.id.exercise);
+        extext.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(Senior_page.this, test.class));
+            }
+        });
+        ImageButton eximage = (ImageButton) findViewById(R.id.excerciseimage);
+        eximage.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(Senior_page.this, test.class));
+            }
+        });
+    }
+
+    public void seniormentalhealthclick(View v) {
+
+        TextView seniormentalhealthtext = (TextView) findViewById(R.id.seniormentalhealth);
+        seniormentalhealthtext.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(Senior_page.this, test.class));
+            }
+        });
+        ImageButton seniormentalhealthimage = (ImageButton) findViewById(R.id.mentalhealth);
+        seniormentalhealthimage.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(Senior_page.this, test.class));
+            }
+        });
+    }
+
+    public void teaclick(View v) {
+
+        TextView teatext = (TextView) findViewById(R.id.tea);
+        teatext.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(Senior_page.this, test.class));
+            }
+        });
+        ImageButton teaimage = (ImageButton) findViewById(R.id.teaimage);
+        teaimage.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(Senior_page.this, test.class));
+            }
+        });
+    }
+
 }
