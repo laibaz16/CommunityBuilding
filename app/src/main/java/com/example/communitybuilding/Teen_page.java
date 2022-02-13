db9ea4f7f2de07003c8c8e522c513651987d91ad
laibaz16 <laibazahid16@gmail.com>
2022-02-12 23:53:48 -0500

    Final Front end of the teens page

--- app/src/main/java/com/example/communitybuilding/Teen_page.java
+++ app/src/main/java/com/example/communitybuilding/Teen_page.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,12 +17,51 @@ public class Teen_page extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teen_page);
     }
-    public void text_msg(View v){
-        TextView teen_btn = (TextView) findViewById(R.id.chitchat);
-        teen_btn.setOnClickListener(new View.OnClickListener()
-        {
-            public void onClick(View v)
-            {
+
+    public void speedclick(View v) {
+        TextView speedtext = (TextView) findViewById(R.id.Speedfriending);
+        speedtext.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(Teen_page.this, test.class));
+            }
+        });
+
+        ImageButton speedimage = (ImageButton) findViewById(R.id.speedimage);
+        speedimage.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(Teen_page.this, test.class));
+            }
+        });
+
+    }
+
+    public void talentshowclick(View v) {
+
+        TextView talenttext = (TextView) findViewById(R.id.talentshow);
+        talenttext.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(Teen_page.this, test.class));
+            }
+        });
+        ImageButton talentshowclick = (ImageButton) findViewById(R.id.talentimage);
+        talentshowclick.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(Teen_page.this, test.class));
+            }
+        });
+    }
+
+    public void paintclick(View v) {
+
+        TextView painttext = (TextView) findViewById(R.id.paint);
+        painttext.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(Teen_page.this, test.class));
+            }
+        });
+        ImageButton talentshowclick = (ImageButton) findViewById(R.id.paintimage);
+        talentshowclick.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
                 startActivity(new Intent(Teen_page.this, test.class));
             }
         });
